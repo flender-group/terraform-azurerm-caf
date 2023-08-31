fc89b7340ef0fa865cc40884d21c4b3d357dc87b
Bastian Karstaedt <2082580+bkarstaedt@users.noreply.github.com>
2023-08-31 15:32:53 +0200

    feat(cognitive_services_account): adds support for private endpoints

--- modules/cognitive_services/cognitive_services_account/variables.tf
+++ modules/cognitive_services/cognitive_services_account/variables.tf
@@ -1,6 +1,10 @@
 variable "global_settings" {
   description = "Global settings object (see module README.md)"
 }
+variable "base_tags" {
+  description = "Base tags for the resource to be inherited from the resource group."
+  type        = bool
+}
 variable "client_config" {
   description = "Client configuration object (see module README.md)."
 }
@@ -16,3 +20,20 @@ variable "resource_group_name" {
 
 variable "settings" {}
 
+variable "vnets" {
+  default = {}
+}
+
+variable "private_endpoints" {
+  default = {}
+}
+
+variable "private_dns" {
+  default = {}
+}
+
+variable "virtual_subnets" {
+  description = "Map of virtual_subnets objects"
+  default     = {}
+  nullable    = false
+}
