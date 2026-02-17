60e31ad0cb23017c17b10607f74e47bbc03bdb41
najeebvv <79649322+najeebvv@users.noreply.github.com>
2026-02-17 09:15:00 +0100

    patch: file system logs (#116)
    - fixed: windows web app file system logs
    
    Co-authored-by: Najeeb Vayal Valappil <najeeb.vayal-valappil.ext@flender-group.com>

--- modules/webapps/windows_web_app/storage_account.tf
+++ modules/webapps/windows_web_app/storage_account.tf
@@ -25,26 +25,6 @@ data "azurerm_storage_account_blob_container_sas" "backup" {
   }
 }
 
-data "azurerm_storage_account_blob_container_sas" "logs" {
-  count = can(var.settings.logs) ? 1 : 0
-
-  connection_string = data.azurerm_storage_account.backup_storage_account.0.primary_connection_string
-  container_name    = local.logs_storage_account.containers[var.settings.logs.container_key].name
-  https_only        = true
-
-  start  = time_rotating.logs_sas[0].id
-  expiry = timeadd(time_rotating.logs_sas[0].id, format("%sh", var.settings.logs.sas_policy.expire_in_days * 24))
-
-  permissions {
-    read   = true
-    add    = true
-    create = true
-    write  = true
-    delete = true
-    list   = true
-  }
-}
-
 
 data "azurerm_storage_account_blob_container_sas" "http_logs" {
   count = can(var.settings.logs.http_logs.azure_blob_storage) ? 1 : 0
@@ -73,15 +53,6 @@ resource "time_rotating" "sas" {
   rotation_days    = lookup(var.settings.backup.sas_policy.rotation, "days", null)
   rotation_months  = lookup(var.settings.backup.sas_policy.rotation, "months", null)
   rotation_years   = lookup(var.settings.backup.sas_policy.rotation, "years", null)
-}
-
-resource "time_rotating" "logs_sas" {
-  count = can(var.settings.logs.sas_policy) ? 1 : 0
-
-  rotation_minutes = lookup(var.settings.logs.sas_policy.rotation, "mins", null)
-  rotation_days    = lookup(var.settings.logs.sas_policy.rotation, "days", null)
-  rotation_months  = lookup(var.settings.logs.sas_policy.rotation, "months", null)
-  rotation_years   = lookup(var.settings.logs.sas_policy.rotation, "years", null)
 }
 
 resource "time_rotating" "http_logs_sas" {
