147838b610f886740412c18dc3ddf725a06ab513
najeebvv <79649322+najeebvv@users.noreply.github.com>
2026-06-17 16:36:37 +0200

    Feat/support import (#122)
    * feat: add additional resource inputs
    
    * support import
    
    ---------
    
    Co-authored-by: Najeeb Vayal Valappil <najeeb.vayal-valappil.ext@flender-group.com>

--- modules/storage_account/storage_account.tf
+++ modules/storage_account/storage_account.tf
@@ -10,7 +10,7 @@ resource "azurecaf_name" "stg" {
   prefixes      = var.global_settings.prefixes
   random_length = var.global_settings.random_length
   clean_input   = true
-  passthrough   = var.global_settings.passthrough
+  passthrough   = can(var.storage_account.passthrough) ? var.storage_account.passthrough : var.global_settings.passthrough
   use_slug      = var.global_settings.use_slug
 }
 
